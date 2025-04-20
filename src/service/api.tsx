979ce63a3a804830892fa763aa8a29d9e1164316
xavierruth <ruthmarianne.xavier@gmail.com>
2025-04-19 23:54:13 -0300

    conectado com o render

--- src/service/api.tsx
+++ src/service/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://fraudi-backend.onrender.com',
     headers: {
         'Content-Type': 'application/json',
     },
